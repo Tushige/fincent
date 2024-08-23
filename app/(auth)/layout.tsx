3ee43c6add13c42973383250dccf4761415b131f
Tushige <tushige2000@gmail.com>
2024-08-23 17:26:09 -0500

    bootstrapped project with shadcnui and next

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+      {children}
+    </main>
+  );
+}
+
+
+export default AuthLayout
